ef6d34c4d83c88f7ae13af0a4f49bd9a207fbf22
marfrancis <marcelomediasolution@gmail.com>
2019-09-30 14:31:41 -0300

    dashboard

---
+++ js/alerta.js
@@ -0,0 +1,18 @@
+function cadastro(){
+
+    Swal.fire({
+        title: 'Obrigado pelo cadastro!',
+        text: "Em breve um consultor entrará em contato com você",
+        type: 'success',
+        showCancelButton: false,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Navegar pelo site!'
+      }).then((result) => {
+        if (result.value) {
+            window.location.assign("oficinas.html");
+
+        }
+      })
+
+}
